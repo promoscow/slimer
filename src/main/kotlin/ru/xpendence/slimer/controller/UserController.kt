ba571ba86e13600f6708e6ba643effa849fb1b08
Vyacheslav Chernyshoff <v.chernyshov@pflb.ru>
2019-06-21 19:18:06 +0300

    Revert: + добавил аннотации и абстракции ? все поля null

---
+++ src/main/kotlin/ru/xpendence/slimer/controller/UserController.kt
@@ -0,0 +1,35 @@
+package ru.xpendence.slimer.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import ru.xpendence.slimer.dto.UserDto
+import ru.xpendence.slimer.service.UserServiceImpl
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 2019-06-12
+ * Time: 09:34
+ * e-mail: 2262288@gmail.com
+ */
+@RestController
+@RequestMapping("/user")
+class UserController @Autowired constructor(private val service: UserServiceImpl) {
+
+    @PostMapping
+    fun create(@RequestBody dto: UserDto?): ResponseEntity<UserDto?> = ResponseEntity.ok(service.save(dto)!!)
+
+    @PutMapping
+    fun update(@RequestBody dto: UserDto?): ResponseEntity<UserDto?> = ResponseEntity.ok(service.update(dto)!!)
+
+    @GetMapping
+    fun get(@RequestParam("id") id: Long): ResponseEntity<UserDto?> = ResponseEntity.ok(service.get(id)!!)
+
+    @GetMapping("/all")
+    fun getAll(pageable: Pageable): ResponseEntity<Page<UserDto>> = ResponseEntity.ok(service.getAll(pageable))
+
+    @DeleteMapping
+    fun delete(@RequestParam(value = "id") id: Long): ResponseEntity<Boolean> = ResponseEntity.ok(service.delete(id))
+}
