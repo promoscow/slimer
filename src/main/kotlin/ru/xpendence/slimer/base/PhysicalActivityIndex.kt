6b9590f8116dbde6205eaae6d897f5836e243c16
v.chernyshoff <slava_rossii@list.ru>
2019-06-22 17:06:30 +0300

    * вожусь с PAI

---
+++ src/main/kotlin/ru/xpendence/slimer/base/PhysicalActivityIndex.kt
@@ -0,0 +1,19 @@
+package ru.xpendence.slimer.base
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 22.06.19
+ * Time: 16:40
+ * e-mail: v.chernyshov@pflb.ru
+ */
+enum class PhysicalActivityIndex {
+    VERY_LOW, LOW, AVERAGE, HIGH
+}
+
+val indexes: HashMap<PhysicalActivityIndex, HashMap<Gender, Double>> =
+        hashMapOf(
+                PhysicalActivityIndex.VERY_LOW to hashMapOf(Gender.FEMALE to 1.3, Gender.MALE to 1.3),
+                PhysicalActivityIndex.LOW to hashMapOf(Gender.FEMALE to 1.4, Gender.MALE to 1.4),
+                PhysicalActivityIndex.AVERAGE to hashMapOf(Gender.FEMALE to 1.6, Gender.MALE to 1.7),
+                PhysicalActivityIndex.HIGH to hashMapOf(Gender.FEMALE to 1.8, Gender.MALE to 1.9)
+        )
