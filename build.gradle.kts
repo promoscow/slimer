1df42f6668f9911c55256ab36bb96ede438cd9de
xpendence <slava_rossii@list.ru>
2019-06-29 12:39:08 +0300

    - xlsx parser, maven dependency and file

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.apache.poi:poi-ooxml:3.17")
     
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
